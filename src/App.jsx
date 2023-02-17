015c67ee19058b36a0b7b0a1f2225a78626091d8
webpawan <pawansaini66477@gmail.com>
2023-02-17 10:57:47 +0530

    tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-gray-400">
+        Hello world!
+      </h1>
+    </>
+  );
+};
+
+export default App;
