3953e191b25fd9179b4a163da03048d71c2c7de1
webpawan <pawansaini66477@gmail.com>
2023-03-20 22:20:16 +0530

    bux fixing while hosting clien side 1.1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      " https://main--chatappclone01.netlify.app/api/":
+      "/api/":
         "https://chatclone-6e9i.onrender.com/",
     },
   },
